0b30ebfc5b8da84e10be3c1cab37c61bd72cc1c9
AVespaIsNotAMotorcycle <sasha.m.ebersole@gmail.com>
2025-09-05 09:58:24 -0400

    Added icons to sidebar

--- frontend/app/components/Sidebar.tsx
+++ frontend/app/components/Sidebar.tsx
@@ -1,12 +1,27 @@
+import EmailIcon from '@mui/icons-material/Email';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import HomeIcon from '@mui/icons-material/Home';
+
+const buttons = [
+	{ icon: <HomeIcon />, label: 'Home' },
+  { icon: <AccountBoxIcon />, label: 'Profile' },
+  { icon: <EmailIcon />, label: 'Messages' },
+];
+
 export default function Header() {
   return (
 	  <header>
 			<span className="logo">MIMIC</span>
 			<nav>
 				<ul>
-					<li><a href="home">Home</a></li>
-					<li><a href="profile">Profile</a></li>
-					<li><a href="messages">Messages</a></li>
+					{buttons.map(({ icon, label }) => (
+					  <li key={label}>
+							<a href={label}>
+								{icon}
+								{label}
+					  	</a>
+					  </li>
+					))}
 				</ul>
 			</nav>
 		</header>
