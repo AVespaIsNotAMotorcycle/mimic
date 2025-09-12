23b2d8c5d0e5f1324123b16e7b9eb95fe833afc4
AVespaIsNotAMotorcycle <sasha.m.ebersole@gmail.com>
2025-09-12 16:48:31 -0400

    Improving form validation

--- frontend/app/components/Input.tsx
+++ frontend/app/components/Input.tsx
@@ -1,3 +1,6 @@
+import CheckIcon from '@mui/icons-material/Check';
+import WarningAmberIcon from '@mui/icons-material/WarningAmber';
+
 import './Input.css';
 
 function Field({
@@ -12,6 +15,27 @@ function Field({
 	}
 }
 
+function Requirements({ value, requirements }) {
+  if (!requirements) return null;
+	console.log(requirements[0]);
+	return (
+		<div className="input-requirements">
+		  {requirements.map(([description, checker]) => {
+				if (typeof checker !== 'function') {
+					console.error(`Checker associated with description "${description}" is not a function.`);
+					return null;
+				}
+				return (
+					<div className={checker(value) ? 'yes' : 'no'}>
+						{checker(value) ? <CheckIcon /> : <WarningAmberIcon />}
+						{description}
+					</div>
+				);
+			})}
+		</div>
+	);
+}
+
 interface InputProps {
 	inputID: string,
 	label: string,
@@ -20,11 +44,24 @@ interface InputProps {
 }
 
 export default function Input(props: InputProps) {
-	const { inputID, label, type, value = '', onChange } = props;
+	const {
+		inputID,
+		label,
+		type,
+		value = '',
+		onChange,
+		requirements,
+	} = props;
 	return (
 		<label htmlFor={inputID}>
 			<span className="label-text">{label}</span>
-			<Field type={type} inputID={inputID} value={value} onChange={onChange} />
+			<Field
+				type={type}
+				inputID={inputID}
+				value={value}
+				onChange={onChange}
+			/>
+			<Requirements value={value} requirements={requirements} />
 		</label>
 	);
 }
--- frontend/app/sign_up/page.tsx
+++ frontend/app/sign_up/page.tsx
@@ -1,3 +1,7 @@
+'use client'
+
+import { useState } from 'react';
+
 import Input from '../components/Input';
 
 const FORM = {
@@ -18,6 +22,20 @@ const FORM = {
 	password1: {
 		type: 'password',
 		label: 'Password',
+		requirements: [
+			[
+				'Password must be at least 6 characters',
+				(value) => value.length >= 6,
+			],
+			[
+				'Password must contain a capital letter',
+				(value) => 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('').find((letter) => value.includes(letter)),
+			],
+			[
+				'Password must contain a number',
+				(value) => '0123456789'.split('').find((letter) => value.includes(letter)),
+			],
+		],
 	},
 	password2: {
 		type: 'password',
@@ -25,11 +43,51 @@ const FORM = {
 	},
 };
 
+const FORM_ID = 'signup_form';
+
 export default function SignUp() {
+	const [formData, setFormData] = useState({});
+
+	async function onSubmit(event: FormEvent<HTMLFormElement>) {
+		event.preventDefault();
+
+		console.log(formData);
+	}
+
+	const updateForm = (key, value) => {
+		console.log(key, value);
+		setFormData({ ...formData, [key]: value });
+	};
+
+	if (formData.password1) {
+		FORM.password2.requirements = [[
+			'Passwords must match',
+			(value) => value === formData.password1,
+		]];
+	} else {
+		FORM.password2.requirements = undefined;
+	}
+	if (formData.email1) {
+		FORM.email2.requirements = [[
+			'Emails must match',
+			(value) => value === formData.email1,
+		]];
+	} else {
+		FORM.email2.requirements = undefined;
+	}
+
   return (
-		<form>
-			{Object.entries(FORM).map(([key, { label, type}]) => (
-				<Input key={key} inputID={key} label={label} type={type} />
+		<form onSubmit={onSubmit} id={FORM_ID}>
+			{Object.entries(FORM).map(([key, { label, type, requirements }]) => (
+				<Input
+					key={key}
+					inputID={key}
+					label={label}
+					type={type}
+					value={formData[key]}
+					onChange={({ target }) => { updateForm(key, target.value); }}
+					requirements={requirements}
+				/>
 			))}
 			<button type="submit">Submit</button>
 		</form>
