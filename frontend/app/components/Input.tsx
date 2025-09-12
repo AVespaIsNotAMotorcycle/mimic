547a0cb3d59383f2c48d9ea88aa923249c23055d
AVespaIsNotAMotorcycle <sasha.m.ebersole@gmail.com>
2025-09-12 16:09:41 -0400

    Added support for controlled inputs

--- frontend/app/components/Input.tsx
+++ frontend/app/components/Input.tsx
@@ -3,9 +3,12 @@ import './Input.css';
 function Field({
 	type,
 	inputID,
+	value,
+	onChange,
 }) {
   switch (type) {
-		default: return <input type={type} id={inputID} />
+		default:
+			return <input type={type} id={inputID} value={value} onChange={onChange} />;
 	}
 }
 
@@ -13,14 +16,15 @@ interface InputProps {
 	inputID: string,
 	label: string,
 	type: string,
+  value: any,
 }
 
 export default function Input(props: InputProps) {
-	const { inputID, label, type } = props;
+	const { inputID, label, type, value = '', onChange } = props;
 	return (
 		<label htmlFor={inputID}>
 			<span className="label-text">{label}</span>
-			<Field type={type} inputID={inputID} />
+			<Field type={type} inputID={inputID} value={value} onChange={onChange} />
 		</label>
 	);
 }
