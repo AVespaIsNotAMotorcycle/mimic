6d0fa29e736936fbe706200a738af4499cccf439
AVespaIsNotAMotorcycle <sasha.m.ebersole@gmail.com>
2025-09-12 13:55:51 -0400

    Added hashPassword function

--- backend/api/app.js
+++ backend/api/app.js
@@ -1,30 +1,14 @@
 const { MongoClient } = require('mongodb');
 const express = require('express')
 const cors = require('cors')
+const { createHash } = require('crypto');
+
 const app = express()
 const port = 8000
 
 const client = new MongoClient(process.env.MIMIC_DB_CONNECTION_STRING);
 
 app.use(cors())
-
-const USERS = {
-  test_poster: {
-		userName: 'test_poster',
-		displayName: 'John Doe',
-		profilePicture: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/Egon_Schiele_-_Self-Portrait_with_Physalis_-_Google_Art_Project.jpg/330px-Egon_Schiele_-_Self-Portrait_with_Physalis_-_Google_Art_Project.jpg',
-	},
-	not_a_real_human: {
-		userName: 'not_a_real_human',
-		displayName: 'Hugh Mann',
-		profilePicture: 'https://static01.nyt.com/images/2024/08/06/multimedia/02schiele-qbpl/02schiele-qbpl-superJumbo.jpg',
-	},
-	someones_dog: {
-		userName: 'someones_dog',
-		displayName: 'Internet Dog',
-		profilePicture: 'https://6.api.artsmia.org/800/10219.jpg',
-	},
-};
 
 const POSTS = [
 		{
@@ -63,6 +47,12 @@ app.get('/posts', (req, res) => {
 
 function getPostsFromUser(user) {
   return POSTS.filter(({ userName }) => userName === user);
+}
+
+function hashPassword(password) {
+	const hash = createHash('sha256');
+	hash.update(password);
+	return hash.digest('hex');
 }
 
 app.get('/user/:userName', async (req, res) => {
