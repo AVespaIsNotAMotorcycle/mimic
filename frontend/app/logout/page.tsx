c274df70f9dc2fc94ba9d3c5a7155658228cda2c
AVespaIsNotAMotorcycle <sasha.m.ebersole@gmail.com>
2025-09-16 13:46:36 -0400

    Can log out

---
+++ frontend/app/logout/page.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import { useEffect } from 'react';
+
+export default function LogOut() {
+	useEffect(() => {
+		localStorage.removeItem('userName');
+		localStorage.removeItem('authKey');
+		window.location.assign('..');
+	}, [])
+
+  return (
+		<h1>Logging out...</h1>
+	);
+}
