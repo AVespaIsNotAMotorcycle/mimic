be9c419603bb413bdae1b0dd30083eac9f4e6b39
AVespaIsNotAMotorcycle <sasha.m.ebersole@gmail.com>
2025-09-16 15:55:39 -0400

    Fixed key for posts

--- frontend/app/components/Timeline.tsx
+++ frontend/app/components/Timeline.tsx
@@ -76,7 +76,7 @@ export default async function Timeline() {
   return (
 	  <>
 			<ComposePost />
-		  {posts.map((post) => <PostDisplay key={post.id} {...post} />)}
+		  {posts.map((post) => <PostDisplay key={post._id} {...post} />)}
 		</>
 	);
 }
