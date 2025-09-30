2d9618bc1105c6fb020b12372ac5b472851b1028
AVespaIsNotAMotorcycle <sasha.m.ebersole@gmail.com>
2025-09-30 12:47:19 -0400

    Implemented stronger typing, changed from img to the nextJS Image component, and added alt text

--- frontend/app/components/Image.tsx
+++ frontend/app/components/Image.tsx
@@ -1,4 +1,16 @@
-function getURL(pathType, source) {
+import NextJSImage from 'next/image';
+
+interface Props {
+	source: string,
+	pathType: 'userName' | 'relativeToImages',
+	className?: string,
+	alt: '',
+}
+
+function getURL(
+	pathType: Props['pathType'],
+	source: Props['source'],
+) {
 	const prePath = 'http://localhost:8000';
 	switch (pathType) {
 		case 'userName': return `${prePath}/user/${source}/profilePicture`;
@@ -6,10 +18,18 @@ function getURL(pathType, source) {
 	}
 }
 
-export default function Image({
-	source,
-	className,
-	pathType = 'relativeToImages',
-}) {
-	return <img src={getURL(pathType, source)} className={className} />;
+export default function Image(props: Props) {
+	const {
+		source,
+		className,
+		pathType = 'relativeToImages',
+		alt,
+	} = props;
+	return (
+		<NextJSImage
+			src={getURL(pathType, source)}
+			className={className}
+			alt={alt}
+		/>
+	);
 }
