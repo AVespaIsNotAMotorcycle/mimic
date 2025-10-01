7bd540c9f3233c724323ecb172438ce64ea28dc7
AVespaIsNotAMotorcycle <sasha.m.ebersole@gmail.com>
2025-10-01 11:12:15 -0400

    Added typing to Box.tsx

--- frontend/app/components/Box.tsx
+++ frontend/app/components/Box.tsx
@@ -1,4 +1,10 @@
-export default function Box({ className, children }) {
+import { ReactNode } from 'react';
+
+export default function Box(props: {
+	className : string;
+	children : ReactNode;
+}) {
+	const { className, children } = props;
 	return (
 		<div className={`${className} box`}>
 			{children}
